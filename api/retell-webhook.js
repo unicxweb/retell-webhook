eb6b9e9ac1c1f42715b15ce520460e28556ccac9
unicxweb <unicx.web@gmail.com>
2025-11-18 16:46:46 +0530

    Update webhook handler to log POST requests

--- api/retell-webhook.js
+++ api/retell-webhook.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
-  console.log("Incoming webhook from Retell:", req.body);
+  if (req.method === "POST") {
+    console.log("Webhook received:", req.body);
+    return res.status(200).json({ message: "Webhook received" });
+  }
 
-  // Always respond success
-  res.status(200).json({ received: true });
+  res.status(200).send("Retell webhook is live!");
 }
